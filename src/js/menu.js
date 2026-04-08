59bebb80cf26447af2434d26e1b0fd4ce2ba7f5c
Bastian Abi Manyu <bastianabimanyu010105@gmail.com>
2026-04-08 12:34:03 +0700

    updategambar

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,4 +1,40 @@
-import cheeseburger from "../assets/image-menu/image-cheeseburger.png";
+// Import Data Gambar Burger //
+import cheeseburger           from "../assets/image-menu/image-cheeseburger.png";
+import doublebeefburger       from "../assets/image-menu/image-doublebeefburger.png";
+import chickencrispyburger    from "../assets/image-menu/image-chickencrispyburger.png";
+import spicyburger            from "../assets/image-menu/image-spicyburger.png";
+
+// Import Data Gambar Pizza //
+import pepperonipizza         from "../assets/image-menu/image-pepperonipizza.png";
+import cheesepizza            from "../assets/image-menu/image-cheesepizza.png";
+import meatlovers             from "../assets/image-menu/image-meatlovers.png";
+import veggiepizza            from "../assets/image-menu/image-veggiepizza.png";
+
+// Import Data Gambar Fruit //
+import freshapple            from "../assets/image-menu/image-freshapple.png";
+import banana                from "../assets/image-menu/image-banana.png";
+import orange                from "../assets/image-menu/image-orange.png";
+import strawberry            from "../assets/image-menu/image-strawberry.png";
+
+// Import Data Gambar Salad //
+import caesarsalad           from "../assets/image-menu/image-caesarsalad.png";
+import fruitsalad            from "../assets/image-menu/image-fruitsalad.png";
+import healthyveggiesalad    from "../assets/image-menu/image-healthyveggiesalad.png";
+import chickensalad          from "../assets/image-menu/image-chickensalad.png";
+
+// Import Data Gambar Salad //
+import orangejuice           from "../assets/image-menu/image-orangejuice.png";
+import coffee                from "../assets/image-menu/image-coffee.png";
+import chocolatemilkshake    from "../assets/image-menu/image-chocolatemilkshake.png";
+import icetea                from "../assets/image-menu/image-icetea.png";
+
+// Import Data Gambar Fried Rice //
+import nasigorengoriginal    from "../assets/image-menu/image-nasigorengoriginal.png";
+import nasigorengseafood     from "../assets/image-menu/image-nasigorengseafood.png";
+import nasigorengpedas       from "../assets/image-menu/image-nasigorengpedas.png";
+import nasigorengtelur       from "../assets/image-menu/image-nasigorengtelur.png";
+
+
 
 // Data Product Burger //
 export const burger = [
@@ -11,21 +47,21 @@ export const burger = [
   },
   {
     id: 2,
-    image: "../src/assets/image-menu/image-doublebeefburger.png",
+    image: doublebeefburger,
     title: "Double Beef Burger",
     berat: "160g",
     harga: "Rp32.000"
   },
   {
     id: 3,
-    image: "../src/assets/image-menu/image-chickencrispyburger.png",
+    image: chickencrispyburger,
     title: "Chicken Crispy Burger",
     berat: "165g",
     harga: "Rp28.000"
   },
   {
     id: 4,
-    image: "../src/assets/image-menu/image-spicyburger.png",
+    image: spicyburger,
     title: "Spicy Burger",
     berat: "155g",
     harga: "Rp27.000"
@@ -37,28 +73,28 @@ export const burger = [
 export const pizza = [
   {
     id: 5,
-    image: "../src/assets/image-menu/image-pepperonipizza.png",
+    image: pepperonipizza,
     title: "Pepperoni Pizza",
     berat: "250g",
     harga: "Rp45.000"
   },
   {
     id: 6,
-    image: "../src/assets/image-menu/image-cheesepizza.png",
+    image: cheesepizza,
     title: "Cheese Pizza",
     berat: "230g",
     harga: "Rp40.000"
   },
   {
     id: 7,
-    image: "../src/assets/image-menu/image-meatlovers.png",
+    image: meatlovers,
     title: "Meat Lovers",
     berat: "270g",
     harga: "Rp50.000"
   },
   {
     id: 8,
-    image: "../src/assets/image-menu/image-veggiepizza.png",
+    image: veggiepizza,
     title: "Veggie Pizza",
     berat: "240g",
     harga: "Rp42.000"
@@ -70,28 +106,28 @@ export const pizza = [
 export const fruit = [
   {
     id: 9,
-    image: "../src/assets/image-menu/image-freshapple.png",
+    image: freshapple,
     title: "Fresh Apple",
     berat: "150g",
     harga: "Rp10.000"
   },
   {
     id: 10,
-    image: "../src/assets/image-menu/image-banana.png",
+    image: banana,
     title: "Banana",
     berat: "130g",
     harga: "Rp8.000"
   },
   {
     id: 11,
-    image: "../src/assets/image-menu/image-orange.png",
+    image: orange,
     title: "Orange",
     berat: "140g",
     harga: "Rp9.000"
   },
   {
     id: 12,
-    image: "../src/assets/image-menu/image-strawberry.png",
+    image: strawberry,
     title: "Strawberry",
     berat: "120g",
     harga: "Rp15.000"
@@ -103,28 +139,28 @@ export const fruit = [
 export const salad = [
   {
     id: 13,
-    image: "../src/assets/image-menu/image-caesarsalad.png",
+    image: caesarsalad,
     title: "Caesar Salad",
     berat: "180g",
     harga: "Rp30.000"
   },
   {
     id: 14,
-    image: "../src/assets/image-menu/image-fruitsalad.png",
+    image: fruitsalad,
     title: "Fruit Salad",
     berat: "170g",
     harga: "Rp28.000"
   },
   {
     id: 15,
-    image: "../src/assets/image-menu/image-healthyveggiesalad.png",
+    image: healthyveggiesalad,
     title: "Healthy Veggie Salad",
     berat: "160g",
     harga: "Rp26.000"
   },
   {
     id: 16,
-    image: "../src/assets/image-menu/image-chickensalad.png",
+    image: chickensalad,
     title: "Chicken Salad",
     berat: "200g",
     harga: "Rp35.000"
@@ -136,28 +172,28 @@ export const salad = [
 export const drink = [
   {
     id: 17,
-    image: "../src/assets/image-menu/image-orangejuice.png",
+    image: orangejuice,
     title: "Orange Juice",
     berat: "300ml",
     harga: "Rp18.000"
   },
   {
     id: 18,
-    image: "../src/assets/image-menu/image-coffee.png",
+    image: coffee,
     title: "Coffee",
     berat: "250ml",
     harga: "Rp15.000"
   },
   {
     id: 19,
-    image: "../src/assets/image-menu/image-chocolatemilkshake.png",
+    image: chocolatemilkshake,
     title: "Chocolate Milkshake",
     berat: "350ml",
     harga: "Rp22.000"
   },
   {
     id: 20,
-    image: "../src/assets/image-menu/image-icetea.png",
+    image: icetea,
     title: "Ice Tea",
     berat: "300ml",
     harga: "Rp12.000"
@@ -169,28 +205,28 @@ export const drink = [
 export const friedrice = [
   {
     id: 21,
-    image: "../src/assets/image-menu/image-nasigorengoriginal.png",
+    image: nasigorengoriginal,
     title: "Nasi Goreng Original",
     berat: "250g",
     harga: "Rp22.000"
   },
   {
     id: 22,
-    image: "../src/assets/image-menu/image-nasigorengseafood.png",
+    image: nasigorengseafood,
     title: "Nasi Goreng Seafood",
     berat: "270g",
     harga: "Rp30.000"
   },
   {
     id: 23,
-    image: "../src/assets/image-menu/image-nasigorengpedas.png",
+    image: nasigorengpedas,
     title: "Nasi Goreng Pedas",
     berat: "260g",
     harga: "Rp24.000"
   },
   {
     id: 24,
-    image: "../src/assets/image-menu/image-nasigorengtelur.png",
+    image: nasigorengtelur,
     title: "Nasi Goreng Telur",
     berat: "240g",
     harga: "Rp20.000"
